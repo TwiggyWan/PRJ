fea5bf2a08700b2ec76b21471d35e9ef9dd0d7c4
Anis SEMMAR <anis.semmar@gmail.com>
2014-03-22 16:55:24 +0100

    Signed-off-by: Anis SEMMAR <anis.semmar@gmail.com>

--- source/main.cpp
+++ source/main.cpp
@@ -16,193 +16,165 @@
 #include "opencv2/nonfree/features2d.hpp"
 #include "opencv2/flann/flann.hpp"
 
-using namespace cv;
-using namespace std;
 
 
-int main(int argc, char** argv) 
-{
-    
+int main(int argc, char** argv) {
+
     cvNamedWindow("Result");
-    
-    initModule_features2d();
 
-    //open camera, check if succeeded
-    VideoCapture capWebCam(0);
-    if(!capWebCam.isOpened())
-    {
-        cout<<"error accessing to camera data!"<<endl;
+    cv::initModule_features2d();
+
+    //get the frames
+
+    if (argc != 3) {
+        std::cout << "error : please specify the required number of arguments!" << std::endl;
         return -1;
     }
-         
+
+    cv::Mat img_reference = cv::imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+    cv::Mat img_current = cv::imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
+
     //reference frame computation
-    
-    //get reference frame
-    Mat img_reference;    
-    if(!capWebCam.read(img_reference))
-    {
-        cout<<"error getting the frame"<<endl;
-        return -2;
-    }
-    
-    imshow("reference",img_reference);
-    
-    //transform it to grayscale
-    cvtColor(img_reference,img_reference,CV_RGB2GRAY);
-    
+    cv::imshow("reference", img_reference);
     //detect keypoints
-    vector<KeyPoint> kp_ref;
-    SurfFeatureDetector detector(500); //the higher the threshold the less points we get
-    
-    detector.detect(img_reference,kp_ref);
+    std::vector<cv::KeyPoint> kp_ref;
+    cv::SurfFeatureDetector detector(500); //the higher the threshold the less points we get
+    detector.detect(img_reference, kp_ref);
     //drawKeypoints(reference,kp_ref,out,Scalar(255,255,255),DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    
-    
+
+
     //get descriptors
-    SurfDescriptorExtractor extractor;
-    Mat descriptors_reference;
-    extractor.compute(img_reference,kp_ref,descriptors_reference);
-    
-    
+    cv::SurfDescriptorExtractor extractor;
+    cv::Mat descriptors_reference;
+    extractor.compute(img_reference, kp_ref, descriptors_reference);
+
+
     //current frame computation
-    char key = 'a';
-    while(key !='q')
-    {
-       //variables used inside the loop : can i put them inside?
-        Mat img_current;
-        Mat descriptors_current;
-        Mat img_matches;
-        vector<KeyPoint> kp_current;
-        vector<DMatch> matches;
-        vector<DMatch> goodMatches;
-        double max_dist = 0;
-        double min_dist = 100;
-        //get next frame
-        if(!capWebCam.read(img_current))
-        {
-            cout<<"error getting the frame"<<endl;
-            break;
-        }
-        
+    cv::Mat descriptors_current;
+    cv::Mat img_matches;
+    std::vector<cv::KeyPoint> kp_current;
+    std::vector<cv::DMatch> matches;
+    std::vector<cv::DMatch> goodMatches;
+    double max_dist = 0;
+    double min_dist = 100;
 
-        //convert it to gray
-        cvtColor(img_current,img_current,CV_RGB2GRAY);
+    //detect keypoints
+    detector.detect(img_current, kp_current);
 
-        
-        //detect keypoints
-        detector.detect(img_current,kp_current);        
+    //get descriptors
+    extractor.compute(img_current, kp_current, descriptors_current);
 
-        //get descriptors
-        extractor.compute(img_current,kp_current,descriptors_current);
-        
-        //matching
-        FlannBasedMatcher matcher;
-        matcher.match(descriptors_reference,descriptors_current,matches);
-        
-        //find good matches to draw
-        
-        //calculation of max/min distances between keypoints
-        for(int i=0; i<descriptors_reference.rows;i++)
-        {
-                double dist = matches[i].distance;
-                if( dist < min_dist ) min_dist = dist;
-                if( dist > max_dist ) max_dist = dist;
-        
-        }
-        
-        cout<<"Max distance :"<<max_dist<<endl;
-        cout<<"Min distance :"<<min_dist<<endl;
-        
-        //draw only matches for dist<3*min_dist
-        
-        for(int i=0;i < descriptors_reference.rows;i++)
-        {
-            if(matches[i].distance < 3*min_dist)
-            {
-                goodMatches.push_back(matches[i]);
-            }
-        }
-        
-        //display the matches
-        drawMatches(img_reference,kp_ref,img_current,kp_current,
-                goodMatches,img_matches,Scalar::all(-1),Scalar::all(-1),
-                vector<char>(),DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-        
-        
-        //localization
-        vector<Point2f> ref;
-        vector<Point2f> current;
-        
-        for(int i=0;i<goodMatches.size();i++)
-        {
-            //get keypoints from the good matches
-            ref.push_back(kp_ref[goodMatches[i].queryIdx].pt);
-            current.push_back(kp_current[goodMatches[i].trainIdx].pt);
-            
-            
-        }
-        
-        //get homography matrix
-        Mat homography(3,3,CV_64F); //findhomography renvoie une mat de doubles -> 64bits
-        
-        homography = findHomography(ref,current,RANSAC);
-        
-        //compute v and w(omega)
-        
-        //get inverse matrix
-        Mat homographyTransposed;transpose(homography,homographyTransposed);
-        
-        //get 3x3 identity matrix
-        Mat identity = Mat::eye(3,3,CV_64F);
-        
-        
-        
-        //compute task function members
-        
-        //taskV
-        Mat taskV = Mat::zeros(3,3,CV_64F);
-        Mat mStar = Mat::zeros(3,1,CV_64F);
-        
-        mStar.at<double>(0,0)=kp_ref[goodMatches[0].queryIdx].pt.x;
-        mStar.at<double>(0,1)=kp_ref[goodMatches[0].queryIdx].pt.y;
-        mStar.at<double>(0,2)=1.0;
-        
-        taskV = homography - identity;
-        
-        taskV = taskV*mStar;
-        
-        //taskOmega
-        Mat taskOmega = Mat::zeros(3,3,CV_64F);
+    //matching
+    cv::FlannBasedMatcher matcher;
+    matcher.match(descriptors_reference, descriptors_current, matches);
 
-        taskOmega = homography - homographyTransposed;
-        
-        cout<<homography<<"HOMOGRAPHY"<<endl;
-        cout<<homographyTransposed<<"TRANSPOSED"<<endl;
-        cout<<taskOmega<<"TASKOMEGA"<<endl;
-        
-        //compute speed and angular speed
-        float lambdaV=0.1;
-        float lambdaOmega=0.1;
-        
-        Mat speed = Mat::zeros(3,1,CV_64F);
-        Mat angularSpeed = Mat::zeros(3,1,CV_64F);
-        
-        speed = -(lambdaV*identity)*taskV;
-        angularSpeed = - (lambdaOmega*identity)*taskOmega;
-        
-        
-        //show result
-       
-        cout<<speed<<"SPEED"<<endl;
-        cout<<angularSpeed<<"ANGULAR SPEED"<<endl;
-        imshow("Result",img_matches);
-        key=waitKey(1);
-        
+    //find good matches to draw
+
+    //calculation of max/min distances between keypoints
+    for (int i = 0; i < descriptors_reference.rows; i++) {
+
+        double dist = matches[i].distance;
+
+        if (dist < min_dist) min_dist = dist;
+
+        if (dist > max_dist) max_dist = dist;
+
+
     }
-    
 
+    std::cout << "Max distance :" << max_dist << std::endl;
+    std::cout << "Min distance :" << min_dist << std::endl;
+
+    //draw only matches for dist<3*min_dist
+    for (int i = 0; i < descriptors_reference.rows; i++) 
+    {
+        if (matches[i].distance < 3 * min_dist) 
+        {
+            goodMatches.push_back(matches[i]);
+        }
+    }
+
+    //display the matches
+   cv::drawMatches(img_reference, kp_ref, img_current, kp_current,
+            goodMatches, img_matches, cv::Scalar::all(-1), cv::Scalar::all(-1),
+            std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
+    //localization
+    std::vector<cv::Point2f> ref;
+    std::vector<cv::Point2f> current;
+
+    for (int i = 0; i < goodMatches.size(); i++) 
+    {
+        //get keypoints from the good matches
+        ref.push_back(kp_ref[goodMatches[i].queryIdx].pt);
+        current.push_back(kp_current[goodMatches[i].trainIdx].pt);
+
+
+    }
+
+    //get homography matrix
+    cv::Mat homography(3, 3, CV_64F); //findhomography renvoie une mat de doubles -> 64bits
+
+    homography = cv::findHomography(ref, current, cv::RANSAC);
+
+    //compute v and w(omega)
+
+    //get inverse matrix
+    cv::Mat homographyTransposed;
+    cv::transpose(homography, homographyTransposed);
+
+    //get 3x3 identity matrix
+    cv::Mat identity = cv::Mat::eye(3, 3, CV_64F);
+
+
+
+    //compute task function members
+
+    //taskV
+    cv::Mat taskV = cv::Mat::zeros(3, 3, CV_64F);
+    cv::Mat mStar = cv::Mat::zeros(3, 1, CV_64F);
+
+    mStar.at<double>(0, 0) = kp_ref[goodMatches[0].queryIdx].pt.x;
+    mStar.at<double>(0, 1) = kp_ref[goodMatches[0].queryIdx].pt.y;
+    mStar.at<double>(0, 2) = 1.0;
+
+    taskV = homography - identity;
+
+    taskV = taskV*mStar;
+
+    //taskOmega
+    cv::Mat taskOmega = cv::Mat::zeros(3, 3, CV_64F);
+
+    taskOmega = homography - homographyTransposed;
+
+    std::cout << homography << "HOMOGRAPHY" << std::endl;
+    std::cout << homographyTransposed << "TRANSPOSED" << std::endl;
+    std::cout << taskOmega << "TASKOMEGA" << std::endl;
+
+    //compute speed and angular speed
+    float lambdaV = 0.1;
+    float lambdaOmega = 0.1;
+
+    cv::Mat speed = cv::Mat::zeros(3, 1, CV_64F);
+    cv::Mat angularSpeed = cv::Mat::zeros(3, 1, CV_64F);
+
+    speed = -(lambdaV * identity) * taskV;
+    angularSpeed = -(lambdaOmega * identity) * taskOmega;
+
+    //TODO : convertir speed et angularSpeed en params interprétables par le js
     
+    
+    //show result
+    std::cout << speed << "SPEED" << std::endl;
+    std::cout << angularSpeed << "ANGULAR SPEED" << std::endl;
+    cv::imshow("Result", img_matches);
     
     return 0;
+    
 }
+
+
+
+
+
 
